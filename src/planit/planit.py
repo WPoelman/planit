f5424a09e1db4dac55b07ffa8d0691eb4d1705c4
Wessel Poelman <contact+github@wesselpoelman.nl>
2026-02-10 13:12:26 +0100

    individual minutes are not used anymore (just use 00:10) for instance

--- src/planit/planit.py
+++ src/planit/planit.py
@@ -279,7 +279,5 @@ def _parse_slurm_time(time_str: str) -> datetime.timedelta:
         elif time_str.count(":") == 1:
             t = datetime.datetime.strptime(time_str, "%M:%S")
             return datetime.timedelta(minutes=t.minute, seconds=t.second)
-        else:
-            return datetime.timedelta(minutes=int(time_str))
     except ValueError as e:
         raise ValueError(f"Could not parse time '{time_str}'.\n{e}")
